8b5c974edcdef3faae9c40c5855258b32bdedc1c
Alina Terehova <terehovalinas@yandex.ru>
2024-05-24 14:40:12 +0700

    день неделт

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,19 +1,43 @@
+const weekDays = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+
 function showDate() {
-    const weekDays = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
-    const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
-    
     let out = document.getElementById('current-date');
     let today = new Date();
     
-    out.innerHTML = "Дата и время русской локали: " + today.toLocaleString('ru-RU') + "<br>";
-    out.innerHTML += "Дата и время английской локали: " + today.toLocaleString('en') + "<br>";
-    out.innerHTML += "Дата и время греческой локали: " + today.toLocaleString('el-GR') + "<br>";
-    out.innerHTML += "Дата и время испанской локали: " + today.toLocaleString('es') + "<br>";
-    out.innerHTML += "Дата и время финской локали: " + today.toLocaleString('fi') + "<br>";
-    out.innerHTML += "Дата и время французской локали: " + today.toLocaleString('fr') + "<br><br>";
+    let content = "";
+    content += "Дата и время русской локали: " + today.toLocaleString('ru-RU') + "<br>";
+    content += "Дата и время английской локали: " + today.toLocaleString('en') + "<br>";
+    content += "Дата и время греческой локали: " + today.toLocaleString('el-GR') + "<br>";
+    content += "Дата и время испанской локали: " + today.toLocaleString('es') + "<br>";
+    content += "Дата и время финской локали: " + today.toLocaleString('fi') + "<br>";
+    content += "Дата и время французской локали: " + today.toLocaleString('fr') + "<br><br>";
     
-    out.innerHTML += "Текущий год: " + today.getFullYear() + "<br>";
-    out.innerHTML += "Текущий месяц: " + months[today.getMonth()] + "<br>";
-    out.innerHTML += "Текущая дата: " + today.getDate() + "<br>";
-    out.innerHTML += "День недели: " + weekDays[today.getDay()];
+    content += "Текущий год: " + today.getFullYear() + "<br>";
+    content += "Текущий месяц: " + months[today.getMonth()] + "<br>";
+    content += "Текущая дата: " + today.getDate() + "<br>";
+    content += "День недели: " + weekDays[today.getDay()];
+    
+    out.innerHTML = content;
+}
+
+function showDayOfWeek() {
+    const dayInput = document.getElementById('input-day').value;
+    const monthInput = document.getElementById('input-month').value;
+    const yearInput = document.getElementById('input-year').value;
+    
+    // Проверяем, что все поля заполнены
+    if (dayInput && monthInput && yearInput) {
+        const day = parseInt(dayInput);
+        const month = parseInt(monthInput) - 1; // В JavaScript месяцы начинаются с 0
+        const year = parseInt(yearInput);
+        
+        const date = new Date(year, month, day);
+        const dayOfWeek = weekDays[date.getDay()];
+        
+        document.getElementById('day-of-week').innerHTML = "День недели: " + dayOfWeek;
+    } else {
+        // Выводим сообщение об ошибке, если не все поля заполнены
+        document.getElementById('day-of-week').innerHTML = "Ошибка: Введите дату, месяц и год";
+    }
 }
