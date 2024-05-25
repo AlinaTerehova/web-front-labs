02d2829099a79618496dcb5a5b4ca960b7f3fd1d
Alina Terehova <terehovalinas@yandex.ru>
2024-05-25 09:06:22 +0700

    аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -11,3 +11,12 @@ function showSeconds() {
     document.getElementById('time-body').innerHTML = seconds < 10 ? '0' + seconds : seconds;
 }
 setInterval(showSeconds, 1000);
+
+function rotateSecondHand() {
+    let today = new Date();
+    let seconds = today.getSeconds();
+    let degree = seconds * 6; 
+    document.getElementById('second-hand').style.transform = `rotate(${degree}deg)`;
+}
+setInterval(rotateSecondHand, 1000);
+rotateSecondHand(); 
